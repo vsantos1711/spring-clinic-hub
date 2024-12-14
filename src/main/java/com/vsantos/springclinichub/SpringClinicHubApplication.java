8a14f2c55f692eb570cd072f52e572dd345ec5f2
vsantos1711 <vsantos067100@gmail.com>
2024-12-14 10:23:57 -0300

    chore: initial commit

---
+++ src/main/java/com/vsantos/springclinichub/SpringClinicHubApplication.java
@@ -0,0 +1,13 @@
+package com.vsantos.springclinichub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClinicHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClinicHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsantos/spring_clinic_hub/SpringClinicHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsantos.spring_clinic_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClinicHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
